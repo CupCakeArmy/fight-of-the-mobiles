c4ba4eeae5878b387cb5df6591db56ff5a67cf64
nicco <nicco.borgioli@gmail.com>
2018-09-04 20:05:05 +0200

    Flutter changes

--- src/flutter/lib/Screens/Scan.dart
+++ src/flutter/lib/Screens/Scan.dart
@@ -1,29 +1,10 @@
-// import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:qr_mobile_vision/qr_camera.dart';
-// import 'package:flutter/services.dart';
-
-// import 'package:barcode_scan/barcode_scan.dart';
 
 import '../Components/BG.dart';
 import '../Components/SyncIcon.dart';
 
 class Scan extends StatelessWidget {
-  // Future scan() async {
-  //   try {
-  //     String barcode = await BarcodeScanner.scan();
-  //     print('Success');
-  //     print(barcode);
-  //     Navigator.pushNamed(null, '/home');
-  //   } on PlatformException catch (e) {
-  //     if (e.code == BarcodeScanner.CameraAccessDenied)
-  //       print('The user did not grant the camera permission!');
-  //   } catch (e) {
-  //     print('Nope');
-  //     print(e);
-  //   }
-  // }
-
   @override
   Widget build(BuildContext context) => BG(
           child: Center(
